33a799d8ee3339db85390169682c35e87116d30d
rejaneklebis <klebis@gmail.com>
2025-02-19 08:31:37 -0300

    Commit exercício 1

--- primeiralista/routes/web.php
+++ primeiralista/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ex1', function(){
+    return view('lista.ex1');
+});
+
+Route::post('/listaex1', function(Request $request){
+    $nota1 = floatval($request->input('nota1'));
+    $nota2 = floatval($request->input('nota2'));
+    $nota3 = floatval($request->input('nota3'));
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    return view('lista.ex1', compact('media'));
+});
